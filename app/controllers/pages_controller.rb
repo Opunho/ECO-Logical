5422725f71041762e9aa5fc41df00326540e3e40
Johannes Burchard <burchardjohannes@gmail.com>
2021-11-23 15:52:29 +0100

    start on components

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :kitchensink ]
 
   def home
   end
+
+  def kitchensink
+  end
+
 end
