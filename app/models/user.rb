a1a40a9984ef6a14de64cebe301eec6f9f0a2ca5
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2021-11-23 16:54:26 +0100

    api calling from expenses controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  include HTTParty
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
