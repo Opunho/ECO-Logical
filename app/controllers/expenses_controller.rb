a852a81cbd54f23dd48ea302698b39130828f1c7
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2021-11-23 17:46:43 +0100

    api call to mockbank is working

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,13 +3,24 @@ class ExpensesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    base_url = "https://api.mockbank.io/oauth/token"
-    query = { "client_id" => "eco13", "client_secret" => "ecological", "grant_type" => "password",
-              "username" => "demado@zohomail.com", "password" => "EcoLogical13!!" }
-    headers = { "content-type" => "application/json", "authorization" => "Bearer 54a6a795-87fe-4150-aad3-fbf573ce2fa5" }
+    auth_url = "https://api.mockbank.io/oauth/token"
+    auth_query = { "client_id" => "eco13", "client_secret" => "ecological", "grant_type" => "password",
+                   "username" => "demado@zohomail.com", "password" => "EcoLogical13!!" }
+    auth_headers = { "content-type" => "application/json" }
 
-    @user = HTTParty.post(base_url,
-                          query: query,
-                          headers: headers)
+    user = HTTParty.post(auth_url,
+                         query: auth_query,
+                         headers: auth_headers)
+    access_token = user["access_token"]
+
+    base_url = "https://api.mockbank.io/customers/"
+    headers2 = { "content-type" => "application/json", "authorization" => "Bearer #{access_token}" }
+
+    @customers = HTTParty.get(base_url, headers: headers2)
+    @customers.parsed_response["data"].each do |customer|
+      @external_id = customer["externalId"] if customer["username"] == "user2"
+    end
+
+    @transactions = HTTParty.get("#{base_url}#{@external_id}/transactions", headers: headers2)
   end
 end
