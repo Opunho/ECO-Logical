be30c611cc4ee2ed4b94030f5cce211f1a7566af
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2021-11-23 17:56:59 +0100

    expenses controller order in methods

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,18 +3,8 @@ class ExpensesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    auth_url = "https://api.mockbank.io/oauth/token"
-    auth_query = { "client_id" => "eco13", "client_secret" => "ecological", "grant_type" => "password",
-                   "username" => "demado@zohomail.com", "password" => "EcoLogical13!!" }
-    auth_headers = { "content-type" => "application/json" }
-
-    user = HTTParty.post(auth_url,
-                         query: auth_query,
-                         headers: auth_headers)
-    access_token = user["access_token"]
-
     base_url = "https://api.mockbank.io/customers/"
-    headers2 = { "content-type" => "application/json", "authorization" => "Bearer #{access_token}" }
+    headers2 = { "content-type" => "application/json", "authorization" => "Bearer #{api_auth}" }
 
     @customers = HTTParty.get(base_url, headers: headers2)
     @customers.parsed_response["data"].each do |customer|
@@ -26,4 +16,19 @@ class ExpensesController < ApplicationController
 
   def create
   end
+
+  private
+
+  def api_auth
+    auth_url = "https://api.mockbank.io/oauth/token"
+    auth_query = { "client_id" => "eco13", "client_secret" => "ecological", "grant_type" => "password",
+                   "username" => "demado@zohomail.com", "password" => "EcoLogical13!!" }
+    auth_headers = { "content-type" => "application/json" }
+    user = HTTParty.post(auth_url,
+                         query: auth_query,
+                         headers: auth_headers)
+    user["access_token"]
+  end
+
+
 end
