d3bc078a4798873cde7e26903f0a35aefbd4cf9c
Johannes Burchard <burchardjohannes@gmail.com>
2021-11-24 15:24:34 +0100

    fixed reccomendations

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,14 +77,11 @@ puts "Creating Recommendations"
     description: ["We would like to improv e your green life by also helping our planet", "Becoming the green person that you always want to be, helping people around you getting more eco-friendly", "Its better to bike and its healty for you and its saving our planet","Green life is also good life, feel more free to help the planet" ].sample,
     link_to_article: ["https://homeguides.sfgate.com/ecofriendly-mean-78718.html","https://www.britishecologicalsociety.org/publications/journals/ecological-solutions-evidence/","https://www.conserve-energy-future.com/15-easy-ways-to-become-environmentally-friendly.php"].sample
   )
-end
-
-5.times do
-  user_id = @user.id
-  recommendation_id = rand(1..5)
-  @pledge = Pledge.create!(
-    completed: false,
-    user_id: user_id,
-    recommendation_id: recommendation_id
-  )
+    puts "created"
+    user = @user
+    @pledge = Pledge.create!(
+      completed: false,
+      user: user,
+      recommendation: @recommendation
+    )
 end
