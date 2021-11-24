b061170260435279b33e2ba42f9fd0b22a04d0fd
Aleksa Mikic <988gbb@gmail.com>
2021-11-24 14:37:01 +0100

    Pledge seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 
-User.create!(
+@user = User.create!(
   email: 'user@user.com',
   password: 'password',
   first_name: 'user',
@@ -20,5 +20,15 @@ User.create!(
     title: ["Buy greener", "Its greener to be!","Save the Earth", "We love it green", "Why should be green","Dress to impres greener"].sample,
     description: ["We would like to improv e your green life by also helping our planet", "Becoming the green person that you always want to be, helping people around you getting more eco-friendly", "Its better to bike and its healty for you and its saving our planet","Green life is also good life, feel more free to help the planet" ].sample,
     link_to_article: ["https://homeguides.sfgate.com/ecofriendly-mean-78718.html","https://www.britishecologicalsociety.org/publications/journals/ecological-solutions-evidence/","https://www.conserve-energy-future.com/15-easy-ways-to-become-environmentally-friendly.php"].sample
+  )
+end
+
+5.times do
+  user_id = @user.id
+  recommendation_id = rand(1..5)
+  @pledge = Pledge.create!(
+    completed: false,
+    user_id: user_id,
+    recommendation_id: recommendation_id
   )
 end
