d040dc2d335771163bcd0f1e139599d1e4e9208c
Johannes Burchard <burchardjohannes@gmail.com>
2021-11-24 12:16:26 +0100

    simple form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,15 @@
 class ProfilesController < ApplicationController
 
+  def new
+    @pledge = Pledge.new
+  end
+
+  def create
+    @pledge = Pledge.new(pledge_params)
+  end
+
+  def delete
+  end
 
   def show
   end
