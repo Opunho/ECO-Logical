3641c20fcde0ac72ad5b80285d873840294907ac
Johannes Burchard <burchardjohannes@gmail.com>
2021-11-25 15:32:34 +0100

    co2 counter

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,5 +14,6 @@ class ProfilesController < ApplicationController
   def show
     @pledges = Pledge.all
     @completed_pledges = Pledge.where(completed: true)
+    @total_co2_achieved = @completed_pledges.map{|pledge| pledge.recommendation.co2_grams}.sum
   end
 end
