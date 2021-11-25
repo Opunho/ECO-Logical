4c6ab9c2690f6b0f9818b3a005deb669e019693b
Johannes Burchard <burchardjohannes@gmail.com>
2021-11-25 15:03:08 +0100

    123

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_151039) do
+ActiveRecord::Schema.define(version: 2021_11_25_133840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_151039) do
     t.string "link_to_article"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "co2_grams"
   end
 
   create_table "users", force: :cascade do |t|
