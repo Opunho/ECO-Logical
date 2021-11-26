ce19d1d7542f6d6644a74b20755a4233aca231e7
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2021-11-26 14:35:53 +0100

    filter and JS added ajax

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,8 +4,58 @@ class DashboardsController < ApplicationController
     @accounts = current_user.accounts
     @emmissions = current_user.emmissions
     @expense = Expense.new
+    @total_expenses = current_user.user_total_expenses
+    @total_emmissions = (current_user.user_total_emmissions.to_f * current_user.user_total_expenses.to_f) / 1000
+    if params[:month_query].present?
+      @total_expenses = user_total_expenses_month
+      @total_emmissions = total_emmissions_month
+    elsif params[:half_year].present?
+      @total_expenses = user_total_expenses_half_year
+      @total_emmissions = total_emmissions_half_year
+    elsif params[:three_months].present?
+      @total_expenses = user_total_expenses_three_months
+      @total_emmissions = total_emmissions_three_months
+    end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'description.html' }
+    end
   end
 
-  def calculate_emmission
+  def user_total_expenses_month
+    current_user.expenses.last_thirty_days.collect(&:amount).sum(&:to_f)
+  end
+
+  def user_total_expenses_half_year
+    current_user.expenses.last_six_months.collect(&:amount).sum(&:to_f)
+  end
+
+  def user_total_expenses_three_months
+    current_user.expenses.last_three_months.collect(&:amount).sum(&:to_f)
+  end
+
+  def total_emmissions_month
+    total = 0
+    current_user.expenses.last_thirty_days.each do |expense|
+      total += expense.emmission.co2_grams unless expense.emmission.co2_grams.nil?
+    end
+    return total
+  end
+
+  def total_emmissions_half_year
+    total = 0
+    current_user.expenses.last_six_months.each do |expense|
+      total += expense.emmission.co2_grams unless expense.emmission.co2_grams.nil?
+    end
+    return total
+  end
+
+  def total_emmissions_three_months
+    total = 0
+    current_user.expenses.last_three_months.each do |expense|
+      total += expense.emmission.co2_grams unless expense.emmission.co2_grams.nil?
+    end
+    return total
   end
 end
