5237ca4c3e349ca3fd57fad701ecf57b36b70c91
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2021-11-29 10:09:21 +0100

    redirection to dashboard if user signed in added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :kitchensink ]
+  before_action :redirect_if_signed_in
 
   def home
   end
 
   def kitchensink
+  end
+
+  private
+
+  def redirect_if_signed_in
+    redirect_to dashboard_path if user_signed_in?
   end
 
 end
