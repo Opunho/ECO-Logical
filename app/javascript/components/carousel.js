f8fccf5614cc1200f91cb0200add22c499a96c52
Gabriel De Vuyst <gabriel.m.devuyst@gmail.com>
2021-11-29 15:25:39 +0100

    ended the carousel

--- app/javascript/components/carousel.js
+++ app/javascript/components/carousel.js
@@ -1,18 +1,17 @@
-import { preventInsignificantClick } from '@rails/ujs';
 import Swipe from 'swipejs';
 
 const initCarousel = () => {
   const swiper = document.getElementById('mySwipe')
   window.mySwipe = new Swipe(swiper, {
     startSlide: 0,
-    auto: 3000,
+    // auto: 3000,
     draggable: true,
     autoRestart: false,
     continuous: false,
-    disableScroll: false,
-    stopPropagation: false,
+    disableScroll: true,
+    stopPropagation: true,
     callback: function (_index, _swiper) { },
-    // transitionEnd: function (_index, _swiper) { }
+    transitionEnd: function (_index, _swiper) { }
   });
 }
 
